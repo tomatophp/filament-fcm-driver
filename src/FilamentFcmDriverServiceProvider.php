eaa544a17365faf865e6f16008fdae2217f314dd
3x1io <11937812+3x1io@users.noreply.github.com>
2025-05-11 06:03:00 +0000

    Format Code

--- src/FilamentFcmDriverServiceProvider.php
+++ src/FilamentFcmDriverServiceProvider.php
@@ -11,43 +11,43 @@ class FilamentFcmDriverServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        //Register generate command
+        // Register generate command
         $this->commands([
             \TomatoPHP\FilamentFcmDriver\Console\FilamentFcmDriverInstall::class,
         ]);
 
-        //Register Config file
+        // Register Config file
         $this->mergeConfigFrom(__DIR__ . '/../config/filament-fcm-driver.php', 'filament-fcm-driver');
 
-        //Publish Config
+        // Publish Config
         $this->publishes([
             __DIR__ . '/../config/filament-fcm-driver.php' => config_path('filament-fcm-driver.php'),
         ], 'filament-fcm-driver-config');
 
-        //Register Migrations
+        // Register Migrations
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
 
-        //Publish Migrations
+        // Publish Migrations
         $this->publishes([
             __DIR__ . '/../database/migrations' => database_path('migrations'),
         ], 'filament-fcm-driver-migrations');
-        //Register views
+        // Register views
         $this->loadViewsFrom(__DIR__ . '/../resources/views', 'filament-fcm-driver');
 
-        //Publish Views
+        // Publish Views
         $this->publishes([
             __DIR__ . '/../resources/views' => resource_path('views/vendor/filament-fcm-driver'),
         ], 'filament-fcm-driver-views');
 
-        //Register Langs
+        // Register Langs
         $this->loadTranslationsFrom(__DIR__ . '/../resources/lang', 'filament-fcm-driver');
 
-        //Publish Lang
+        // Publish Lang
         $this->publishes([
             __DIR__ . '/../resources/lang' => base_path('lang/vendor/filament-fcm-driver'),
         ], 'filament-fcm-driver-lang');
 
-        //Register Routes
+        // Register Routes
         $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
 
         Livewire::component('filament-fcm-driver', Firebase::class);
